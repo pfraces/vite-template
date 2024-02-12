6a4b838a46cde05913ef15b5949c603756a1ae34
Pau Fracés <pfraces@gmail.com>
2024-02-12 12:02:11 +0100

    added jsconfig.json relative imports to vite

--- root/vite.config.js
+++ root/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   server: {
     port: 3000
-  }
+  },
+  plugins: [tsconfigPaths()]
 });
