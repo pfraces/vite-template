c2137ed42e83ab958db26a2ed37778c99ba7f1d5
Pau Fracés <pfraces@gmail.com>
2024-02-12 11:52:31 +0100

    used vite default project root

--- root/vite.config.js
+++ root/vite.config.js
@@ -1,16 +1,7 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-const dir = (...args) => resolve(__dirname, ...args);
-
 export default defineConfig({
-  root: 'src',
-  publicDir: dir('public'),
   server: {
     port: 3000
-  },
-  build: {
-    outDir: dir('dist'),
-    emptyOutDir: true
   }
 });
